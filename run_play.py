b4e568aaecc0fd1d7a452f9eafcc9256ca3bddf5
Konstantin <mersed@ya.ru>
2022-02-12 12:45:17 +0300

    add random to run_play.py

--- run_play.py
+++ run_play.py
@@ -1,9 +1,22 @@
 import time
+import random
 
 from helium import *
 import undetected_chromedriver as uc
 
 if __name__ =='__main__':
+
+    def func1():
+        url = 'https://v02.tvigle.ru/video/versus/season1/krutoi-protiv-tolpy-vd/?ref=1600'
+        go_to(url)
+
+
+    def func2():
+        print("пукпук2")
+
+
+    def func3():
+        print("пукпук3")
 
     driver = uc.Chrome()
 
@@ -11,9 +24,19 @@ if __name__ =='__main__':
     url = 'https://v02.tvigle.ru/video/versus/season1/krutoi-protiv-tolpy-vd/?ref=1600'
     go_to(url)
 
+    time.sleep(random.randint(30,50))
+
     a = S('.TviglePlayer').exists()
     print(a)
     if a:
         click(S('.TviglePlayer'))
-    time.sleep(1000)
+
+    time.sleep(random.randint(60*60*10,60*60*20))
+
+    getrandom = random.choice([func1, func2, func3])
+    getrandom()
+
     kill_browser()
+
+
+
