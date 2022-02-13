ca2bef8456e1121bd38687df969cdb2abe6b712b
Konstantin <mersed@ya.ru>
2022-02-13 20:48:54 +0300

    add random to run_play.py

--- run_play.py
+++ run_play.py
@@ -6,37 +6,66 @@ import undetected_chromedriver as uc
 
 if __name__ =='__main__':
 
-    def func1():
-        url = 'https://v02.tvigle.ru/video/versus/season1/krutoi-protiv-tolpy-vd/?ref=1600'
-        go_to(url)
+    def func_urls():
+        urls = []
+        urls.append('https://yandex.ru/news/?utm_source=main_stripe_big')
+        urls.append('https://www.google.ru/webhp')
+        urls.append('https://pikabu.ru/')
+        urls.append('https://yandex.ru/')
+
+        go_to(random.choice(urls))
 
 
-    def func2():
-        print("пукпук2")
+    def func_wait():
+        click(S('.TviglePlayer'))
+        time.sleep(random.randint(6, 6 * 10))
+        click(S('.TviglePlayer'))
 
 
-    def func3():
-        print("пукпук3")
+    def func_exit():
+        kill_browser()
+
+
+    def func_ads():
+        click(S('.turn-off-ads'))
+        time.sleep(random.randint(6, 3 * 10))
+        click(S('.close'))
+
 
     driver = uc.Chrome()
 
     set_driver(driver)
     url = 'https://v02.tvigle.ru/video/versus/season1/krutoi-protiv-tolpy-vd/?ref=1600'
     go_to(url)
+    # helium.refresh()
 
-    time.sleep(random.randint(30,50))
+    time.sleep(random.randint(37, 73))
 
     a = S('.TviglePlayer').exists()
-    print(a)
+
+    # b = S('.turn-off-ads').exists()
+
+    # с = S('.close').exists()
+
+    # print(a)
+    # print(b)
+    # print(с)
+
     if a:
+        time.sleep(random.randint(5, 7))
+        #     click(S('.turn-off-ads'))
         click(S('.TviglePlayer'))
+        time.sleep(random.randint(59*10, 59*23))
+        #     click(S('.TviglePlayer'))
+        #     time.sleep(random.randint(6*1,6*2))
 
-    time.sleep(random.randint(60*60*10,60*60*20))
+        getrandom = random.choice([func_urls, func_wait, func_exit, func_ads])
+        getrandom()
+        kill_browser()
 
-    getrandom = random.choice([func1, func2, func3])
-    getrandom()
-
-    kill_browser()
+    else:
+        print('-kill_browser-')
+    #     kill_browser()
 
 
 
